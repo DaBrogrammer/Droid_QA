564d3d40d8a22b4efa42e65c29e6f059fdbeb970
DaBrogrammer <adnan.9821@gmail.com>
2023-08-02 17:23:28 -0400

    Added test file and first push

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# Droid_QA
+# COEN6761 SUMMER PROJECT
+Summer 2023 Testing and validation Project
+### System Prerequisite
+
+#### To Run Application
+To get started with running the project
+
+1. Check the build and dependency status
+   ```shell
+    mvn dependency:resolve
+    mvn package
+   ```
+2. If the dependency issue not resolved, add it externally from [Maven Repository](https://mvnrepository.com/). This issue arises when java version of local and pom.xml is mismatch.
+3. Download jar [javafx(controls,fxml)](https://gluonhq.com/products/javafx/).
+4. Download jar [controlsfx](https://mvnrepository.com/artifact/org.controlsfx/controlsfx/11.1.0)
+5. Download jar [validatorfx](https://mvnrepository.com/artifact/net.synedra/validatorfx/0.1.13)
+6. Go to File -> Project Structure -> Modules -> dependencies and all the externally downloaded jar.
+7. Follow Step 1 again. If dependency issue exists use step 2 to download and add externally.
+8. Use the IDE to run.
+
+#### To Test Application
+
+1. Edit the JUnit run-configuration in IDE to point to all or any of the test cases.
+2. Run the @Test individually by clicking run on the left side of each method.
+
+#### To run Coverage using JaCoCo
+1. In order to generate test cases and the jacoco report run the shell command given below in the project's directory where Pom.xml is present.
+    ```shell
+    mvn clean install
+   ```  
+
+#### UNIVERSITY
+[CONCORDIA UNIVERSITY](https://www.concordia.ca/).
---
+++ WalkingRobot/src/test/java/QATestRobot.java
@@ -0,0 +1,252 @@
+package com.project.walkingrobot;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class QATestRobot {
+
+	private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+	
+	@BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStream));
+    }
+	
+	
+	@Test
+	public void testInitializeSystem_Floor() {
+		Robot robot = new Robot();
+		// test case 1: smaller floor
+		Floor floor = new Floor(3);
+		int[][] expectedFloor = {{0,0,0},{0,0,0},{0,0,0}};
+		Assertions.assertArrayEquals(expectedFloor, floor.getFloorArray());
+		
+		// test case 2: larger floor
+		Floor floor2 = new Floor(10);
+		int[][] expectedFloor2 = {{0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0},
+        		{0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0},
+        		{0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0}};
+		Assertions.assertArrayEquals(expectedFloor2, floor2.getFloorArray());
+		
+	}
+	
+	
+	@Test
+	public void testInitializeSystem_Robot() {
+		// assert that the robot is in position [0, 0], facing north, and with pen up after system initialization
+		Robot robot = new Robot();
+		Floor floor = new Floor(3);
+		Assertions.assertEquals(0, robot.getX());
+		Assertions.assertEquals(0, robot.getY());
+		Assertions.assertFalse(robot.isPenDown());
+		Assertions.assertEquals(Direction.NORTH, robot.getDirection());
+	}
+	
+	
+	@Test
+	public void testSetPenUp() {
+		// assert that robot has the pen up after initialization + when the setPen(boolean) function is called with "false" as input
+		Robot robot = new Robot();
+		Assertions.assertFalse(robot.isPenDown());
+		robot.setPenDown(false);
+		Assertions.assertFalse(robot.isPenDown());
+	}
+	
+	
+	@Test
+	public void testNoDrawing_PenUP() {
+		// test that the robot will not draw on the floor as long as the pen is up
+		Robot robot = new Robot();
+		Floor floor = new Floor(3);
+		
+		robot.setPenDown(false);
+		robot.move(2, floor);
+		robot.turnRight();
+		robot.move(1, floor);
+		
+		int[][] expectedOutput = {{0,0,0},{0,0,0},{0,0,0}};
+		
+		Assertions.assertArrayEquals(expectedOutput, floor.getFloorArray());
+	}
+	
+	
+	@Test
+	public void testSetPenDown() {
+		Robot robot = new Robot();
+		Assertions.assertFalse(robot.isPenDown());
+		robot.setPenDown(true);
+		Assertions.assertTrue(robot.isPenDown());
+	}
+	
+	
+	@Test
+	public void testTurnRight() {
+		Robot robot = new Robot();
+		Floor floor = new Floor(3);
+		
+		robot.turnRight();
+		Assertions.assertEquals(Direction.EAST, robot.getDirection());
+		
+		robot.turnRight();
+		Assertions.assertEquals(Direction.SOUTH, robot.getDirection());
+		
+		robot.turnRight();
+		Assertions.assertEquals(Direction.WEST, robot.getDirection());
+		
+		robot.turnRight();
+		Assertions.assertEquals(Direction.NORTH, robot.getDirection());
+	}
+	
+	
+	@Test
+	public void testTurnLeft() {
+		Robot robot = new Robot();
+		Floor floor = new Floor(3);
+		
+		robot.turnLeft();
+		Assertions.assertEquals(Direction.WEST, robot.getDirection());
+		
+		robot.turnLeft();
+		Assertions.assertEquals(Direction.SOUTH, robot.getDirection());
+		
+		robot.turnLeft();
+		Assertions.assertEquals(Direction.EAST, robot.getDirection());
+		
+		robot.turnLeft();
+		Assertions.assertEquals(Direction.NORTH, robot.getDirection());
+	}
+	
+	
+	@Test
+	public void testMove() {
+		// test case 1: smaller system, smaller movements
+		Robot robot = new Robot();
+		Floor floor = new Floor(3);
+		robot.setPenDown(true);
+		robot.move(2, floor);
+		Assertions.assertEquals(0, robot.getX());
+		Assertions.assertEquals(2, robot.getY());
+		robot.turnRight();
+		robot.move(1, floor);
+		Assertions.assertEquals(1, robot.getX());
+		Assertions.assertEquals(2, robot.getY());
+		
+		// test case 2: larger system, larger movements
+		Robot robot2 = new Robot();
+		Floor floor2 = new Floor(10);
+		robot2.setPenDown(false);
+		robot2.move(6, floor2);
+		Assertions.assertEquals(0, robot2.getX());
+		Assertions.assertEquals(6, robot2.getY());
+		robot2.turnRight();
+		robot2.move(2, floor2);
+		Assertions.assertEquals(2, robot2.getX());
+		Assertions.assertEquals(6, robot2.getY());
+		robot2.turnRight();
+		robot2.move(3, floor2);
+		robot2.move(1, floor2);
+		Assertions.assertEquals(2, robot2.getX());
+		Assertions.assertEquals(2, robot2.getY());
+		
+		// test case 3: untested movements
+		Robot robot3 = new Robot();
+		Floor floor3 = new Floor(5);
+		robot3.setPenDown(true);
+		robot3.turnRight();
+		robot3.move(3, floor3);
+		robot3.turnLeft();
+		robot3.move(2, floor3);
+		robot3.turnLeft();
+		robot3.move(1, floor3);
+		Assertions.assertEquals(2, robot3.getX());
+		Assertions.assertEquals(2, robot3.getY());
+		robot3.turnLeft();
+		robot3.move(1, floor3);
+		Assertions.assertEquals(2, robot3.getX());
+		Assertions.assertEquals(1, robot3.getY());
+		
+	}
+	
+	
+	@Test
+	public void testPrintFloor() {
+		Robot robot = new Robot();
+		Floor floor = new Floor(3);
+		Main main = new Main();
+		robot.setPenDown(true);
+		robot.move(2, floor);
+		robot.turnRight();
+		robot.move(1, floor);
+		
+		int[][] expectedOutput = { {1,1,0},
+								   {1,0,0},
+								   {1,0,0} };
+		
+		// NO EXPLICIT PRINT FUNCTION FOR THE FLOOR
+		// DOES NOT FOLLOW THE SPECIFIC PRINT MODEL GIVEN IN THE PROJECT DESCRIPTION (with "*" replacing 1 and " " replacing 0)
+		// USES A GUI INSTEAD
+		
+		Assertions.assertEquals(expectedOutput, floor.getFloorArray());
+		
+		
+	}
+	
+	
+	@Test
+	public void testPrintCurrentPosition() {
+		// test case 1: print current position after initialization
+		Robot robot = new Robot();
+		Floor floor = new Floor(3);
+		robot.printPosition();
+		Assertions.assertEquals("Position: 0, 0 - Pen: up - Facing: NORTH", outputStream.toString().trim());
+		
+		// test case 2: same system, movement is performed, but pen is up
+		outputStream.reset();
+		robot.setPenDown(false);
+		robot.turnRight();
+		robot.move(2, floor);
+		robot.printPosition();
+		Assertions.assertEquals("Position: 2, 0 - Pen: up - Facing: EAST", outputStream.toString().trim());
+		
+		// test case 3: new system, movement is performed, with pen down
+		outputStream.reset();
+		Robot robot2 = new Robot();
+		Floor floor2 = new Floor(3);
+		robot2.setPenDown(true);
+		robot2.turnRight();
+		robot2.move(1, floor2);
+		robot2.printPosition();
+		Assertions.assertEquals("Position: 1, 0 - Pen: down - Facing: EAST", outputStream.toString().trim());
+		
+		// test case 4: new larger system, larger movements performed, with pen down
+		outputStream.reset();
+		Robot robot3 = new Robot();
+		Floor floor3 = new Floor(10);
+		robot3.setPenDown(true);
+		robot3.move(4, floor3);
+		robot3.turnRight();
+		robot3.move(7, floor3);
+		robot3.turnRight();
+		robot3.printPosition();
+		Assertions.assertEquals("Position: 7, 4 - Pen: down - Facing: SOUTH", outputStream.toString().trim());
+	}
+	
+	
+	@Test
+	public void testEndProgramCommand() {
+		
+	}
+	
+	
+	@Test
+	public void testInvalidCommand() {
+		
+	}
+	
+	
+}
